a3854c25e99c23bc4c5399aa683a2c7b1a5565a4
anathema43 <37859650+anathema43@users.noreply.github.com>
2025-07-31 04:54:19 +0530

    Fix React DOM Client Import Error

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { initializeCostOptimization } from "./utils/costOptimization.js";
@@ -7,7 +7,7 @@ import { initializeCostOptimization } from "./utils/costOptimization.js";
 // Initialize cost optimization measures
 initializeCostOptimization();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
