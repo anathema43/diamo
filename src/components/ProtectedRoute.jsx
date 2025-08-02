27477cbe2496bb7858a30b26714b369ba3c41466
anathema43 <37859650+anathema43@users.noreply.github.com>
2025-08-02 06:12:11 +0530

    Updated ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { useAuthStore } from '../store/authStore';
 import LoadingSpinner from './LoadingSpinner';
 import { saveRedirectPath } from '../utils/redirectUtils';
+import { Navigate } from 'react-router-dom';
+
 export default function ProtectedRoute({ children }) {
   const { currentUser, loading } = useAuthStore();
 
   if (loading) {
     return <LoadingSpinner />;
   }
+
+  if (!currentUser) {
+    saveRedirectPath(window.location.pathname);
     return <Navigate to="/login" replace />;
   }
 
